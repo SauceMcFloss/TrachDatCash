0ddc851a2d953ff6f22deb7e446437bff219ea2c
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-19 18:55:26 -0400

    footer edits

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component{
 		return(
 		
 		<footer className = "footer text-center" >
-			<div className = "container">
+			<div className = "container sticky-bottom">
 				<p className = "footer-text"> Track Dat Ca$h,Group 13</p>
 				
 			</div>
