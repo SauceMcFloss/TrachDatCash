23ced6e84b6a980210be83306021955f51a07836
Luke Hazelton <tomluke600@gmail.com>
2019-04-19 14:32:31 -0400

    Revert "Copy changes from createExpense"
    This reverts commit 59de2b1a46a33b5c29235753ffd9a5e6564df01d.

--- client/src/components/editExpense.js
+++ client/src/components/editExpense.js
@@ -73,10 +73,6 @@ const optionsDay = [
   { value: 31, label: 31 }
 ];
 
-const optionsCode = [
-  { value: '', label: 'No group code' }
-];
-
 class EditExpense extends Component {
 
     constructor(props) {
@@ -100,7 +96,7 @@ class EditExpense extends Component {
             month: '',
             day: '',
             year: '',
-            groupCode: jwt_decode(localStorage.getItem("jwtToken")).groupCode.toString()
+            groupCode: ''
         }
 		
     }
@@ -199,9 +195,6 @@ class EditExpense extends Component {
 		const { selectedCategory } = this.state;
 		const { selectedMonth } = this.state;
 		const { selectedDay } = this.state;
-		const { selectedCode } = this.state;
-		
-		const codeOfUser = jwt_decode(localStorage.getItem("jwtToken")).groupCode;
 	
         return (
             <div className= "App">
@@ -299,13 +292,11 @@ class EditExpense extends Component {
                     </div>
 					<div className="form-group"> 
                         <label>Group Code: </label>
-                        <Select
-							name="Group Code"
-							placeholder={codeOfUser}
-							value={selectedCode}
-							options={optionsCode}
-							onChange={this.onChangeGroupCode}
-						/>
+                        <input  type="text"
+                                className="form-control"
+                                value={this.state.groupCode}
+                                onChange={this.onChangeGroupCode}
+                                />
                     </div>
 
                     <br />
