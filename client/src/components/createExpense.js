1592ada0de17dc47aa332f280ab70f3987bd75cc
Luke Hazelton <tomluke600@gmail.com>
2019-04-19 00:48:19 -0400

    Fix drop down
    There is currently drop down box with one entry in it, but the entry is blank. Hopefully this will fix

--- client/src/components/createExpense.js
+++ client/src/components/createExpense.js
@@ -74,7 +74,7 @@ const optionsDay = [
 ];
 
 const optionsCode = [
-  { value: jwt_decode(localStorage.getItem("jwtToken")).groupCode, label: jwt_decode(localStorage.getItem("jwtToken")).groupCode }
+  { value: jwt_decode(localStorage.getItem("jwtToken")).groupCode.toString(), label: jwt_decode(localStorage.getItem("jwtToken")).groupCode.toString() }
 ];
 
 class CreateExpense extends Component {
