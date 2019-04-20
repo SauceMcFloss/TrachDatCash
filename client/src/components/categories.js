2890241c55d55ed8b79b253dc01bc21bc89223ab
Lexii <37764077+lexiilamb@users.noreply.github.com>
2019-04-20 15:38:24 -0400

    Table description not changing
    Category nav bar should now change "Bills 2019 Total: $" with user selected category

--- client/src/components/categories.js
+++ client/src/components/categories.js
@@ -263,6 +263,7 @@ class TodosList extends Component {
 				sum = sumBy(temp, 'amount');
                 this.setState({ 
 					expensesArray: temp,
+					category: category,
 					total: sum
 				});
             })
