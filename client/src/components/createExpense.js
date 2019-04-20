1320cf409b95766de8982c306587fefa7e1c5f44
Luke Hazelton <tomluke600@gmail.com>
2019-04-19 23:18:40 -0400

    pls
    erxdtcfygvubhijn

--- client/src/components/createExpense.js
+++ client/src/components/createExpense.js
@@ -266,7 +266,7 @@ class CreateExpense extends Component {
 					  <label>Month: </label>
 					  <Select
 						name="Month"
-						defaultValue={optionsMonth[today.getMonth()]}
+						defaultValue=optionsMonth[today.getMonth()].value
 						placeholder={optionsMonth[today.getMonth()].label}
 						value={selectedMonth}
 						options={optionsMonth}
@@ -277,7 +277,7 @@ class CreateExpense extends Component {
 					  <label>Day: </label>
 					  <Select
 						name="Day"
-						defaultValue={optionsDay[today.getDate() - 1]}
+						defaultValue=optionsDay[today.getDate() - 1].value
 						placeholder={optionsDay[today.getDate() - 1].label}
 						value={selectedDay}
 						options={optionsDay}
