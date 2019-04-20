fab0a7eccd041e660ec8c2861fa064d4f6ecc353
dani97daniela <47904871+dani97daniela@users.noreply.github.com>
2019-04-20 18:26:19 -0400

    Update footer.js

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,11 +5,11 @@ class Footer extends React.Component{
 	render(){
 		return(
 		
-		<footer className = "color_back" >
+		<footer className = "footer" >
 			
-				<div className = "footer">
+				
 					<center><p> Track Dat Ca$h, Group 13</p></center>
-				</div>
+				
 			
 		</footer>
 		
