aa51403ba1322ffb244f55c762845ed20cec02e1
Luke Hazelton <tomluke600@gmail.com>
2019-04-21 17:51:05 -0400

    increased delay
    half a second

--- client/src/components/monthly.js
+++ client/src/components/monthly.js
@@ -125,7 +125,7 @@ class TodosList extends Component {
 						budget: response.data.budget,
 						balance: parseFloat(response.data.budget - this.state.total).toFixed( 2 )
 					}),
-					100
+					500
 				)
             })
             .catch(function (error) {
