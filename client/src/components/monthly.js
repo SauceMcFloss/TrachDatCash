5abed6acaacd875cb736f4ef90f2d10a0a167da8
Lexii <37764077+lexiilamb@users.noreply.github.com>
2019-04-20 20:08:01 -0400

    Removed title above table

--- client/src/components/monthly.js
+++ client/src/components/monthly.js
@@ -546,7 +546,6 @@ class TodosList extends Component {
 								  </nav>
 								</div>
 								<div style={{ marginTop: 20 }}>
-									<h5>{this.state.month + " Total: $" + parseFloat(this.state.total).toFixed( 2 )} </h5>
 									<link rel="stylesheet" href="https://npmcdn.com/react-bootstrap-table/dist/react-bootstrap-table-all.min.css"></link>
 									<BootstrapTable 
 										data={this.state.expensesArray} 
